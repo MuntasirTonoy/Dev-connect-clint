4de67b3cdb9b70476fd64eb2f41688ae629cb96b
muntasir.tonoy <tonoy947@gmail.com>
2025-08-17 17:34:55 +0600

    loading fixed

--- src/Component/Loading/Loading.jsx
+++ src/Component/Loading/Loading.jsx
@@ -4,7 +4,7 @@ import animation from "../../assets/Animation/loading.json";
 
 const Loading = () => {
   return (
-    <div className="flex justify-center items-center py-10">
+    <div className="flex justify-center items-center py-10 min-h-screen">
       <div className="w-28 h-28">
         <Lottie animationData={animation} loop={true} autoplay={true} />
       </div>
